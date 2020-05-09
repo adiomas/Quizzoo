b9ad42d8293d846cb40dd33c7fbcf2f178d33709
Adrijan Omićević <adrijan.omicevic@fer.hr>
2020-05-09 22:48:30 +0200

    Initial Commit

---
+++ Quizzoo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quizzoo
+//
+//  Created by Adrijan Omicevic on 09/05/2020.
+//  Copyright © 2020 OmiApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
